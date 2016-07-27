ad18c728af085d53b57c69ccd14423c55812e8f9
Kaitlyn Crawford <kaitlyn@praekelt.com>
2016-07-27 18:33:24 +0200

    Get data from url and format before returning it

--- casepropods/family_connect_registration/plugin.py
+++ casepropods/family_connect_registration/plugin.py
@@ -1,5 +1,7 @@
 from confmodel import fields
+from casepro.cases.models import Case
 from casepro.pods import Pod, PodConfig, PodPlugin
+import requests
 
 
 class RegistrationPodConfig(PodConfig):
@@ -11,7 +13,24 @@ class RegistrationPodConfig(PodConfig):
 
 class RegistrationPod(Pod):
     def read_data(self, params):
-        pass
+        # Setup
+        url = self.config.url
+        token = self.config.token
+        session = requests.Session()
+        session.headers.update({'Authorization': "Token " + token})
+        session.headers.update({'Content-Type': "application/json"})
+        case_id = params["case_id"]
+        case = Case.objects.get(pk=case_id)
+
+        # Get and format registration response
+        r = session.get(url, params={'mother_id': case.contact.uuid})
+        response = r.json()
+        content = {"items": []}
+        for reg in response["results"]:
+            for k in reg:
+                if k != "data":
+                    content['items'].append({"name": k, "value": reg[k]})
+        return content
 
 
 class RegistrationPlugin(PodPlugin):
